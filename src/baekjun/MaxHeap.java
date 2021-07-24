4edc434fde2c120302be133590d2699596925777
sanseongKo <tkstjd56@naver.com>
2021-07-24 16:34:30 +0900

    baekjoon codingtest practice

---
+++ src/baekjun/MaxHeap.java
@@ -0,0 +1,37 @@
+package baekjun;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MaxHeap {
+	public static void main(String[] args) {
+		int[] n = {0,1,2,0,0,3,2,1,0,0,0};
+		int[] tmp = solution(n);
+		for(int i : tmp) {
+			System.out.println(i);
+		}
+	}
+	public static int[] solution(int[] n) {
+		List<Integer> list = new ArrayList<Integer>();
+		PriorityQueue<Integer> que = new PriorityQueue<Integer>(Collections.reverseOrder());
+		for(int i =0; i<n.length;i++) {
+			if(n[i] == 0) {
+				if(que.peek() == null) {
+					list.add(0);
+				}else {
+				list.add(que.poll());
+				}
+			}else {
+				que.add(n[i]);
+				
+			}	
+		}
+		int[] answer = new int[list.size()];
+		for(int i =0; i<list.size();i++) {
+			answer[i] = list.get(i);
+		}
+		return answer;
+	}
+}
