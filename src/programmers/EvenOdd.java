79651dda34ccbc84052842a5da61a84210a7a44b
sanseongKo <tkstjd56@naver.com>
2021-08-21 23:42:04 +0900

    practice

---
+++ src/programmers/EvenOdd.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class EvenOdd {
+	public String solution(int num) {
+        String answer = "";
+        if(num%2 == 1) {
+        	return "Odd";
+        }else {
+        	return "Even";
+        }
+        
+    }
+}
